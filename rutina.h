f8492c560ca13f621b186e756779d9793c458d80
Erick26m <a01715444@tec.mx>
2026-05-26 20:54:29 -0600

    Update rutina.h

--- rutina.h
+++ rutina.h
@@ -7,38 +7,71 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Rutina{
-
+class Rutina {
 private:
-
     string nombre;
     string objetivo;
     int duracionMin;
     string dificultad;
 
 public:
+    Rutina();
+    Rutina(string nombre, string objetivo, int duracionMin, string dificultad);
 
-    Rutina()
-    : nombre("Sin rutina"),
-      objetivo("Sin objetivo"),
-      duracionMin(0),
-      dificultad("Sin dificultad"){}
+    string getNombre();
+    void setNombre(string nombre);
 
-    Rutina(string nom,string obj,int duracion,string dif)
-    : nombre(nom),
-      objetivo(obj),
-      duracionMin(duracion),
-      dificultad(dif){}
+    string getObjetivo();
+    void setObjetivo(string objetivo);
 
-    void mostrarRutina(){
+    int getDuracionMin();
+    void setDuracionMin(int duracionMin);
 
-        cout<<"Rutina: "<<nombre<<endl;
-        cout<<"Objetivo: "<<objetivo<<endl;
-        cout<<"Duracion: "<<duracionMin<<" min"<<endl;
-        cout<<"Dificultad: "<<dificultad<<endl;
+    string getDificultad();
+    void setDificultad(string dificultad);
 
-    }
-
+    string obtenerDescripcion();
 };
+
+Rutina::Rutina()
+    : nombre("Sin rutina"), objetivo("Sin objetivo"), duracionMin(0), dificultad("Sin dificultad") {}
+
+Rutina::Rutina(string nombre, string objetivo, int duracionMin, string dificultad)
+    : nombre(nombre), objetivo(objetivo), duracionMin(duracionMin), dificultad(dificultad) {}
+
+string Rutina::getNombre() {
+    return nombre;
+}
+
+void Rutina::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+string Rutina::getObjetivo() {
+    return objetivo;
+}
+
+void Rutina::setObjetivo(string objetivo) {
+    this->objetivo = objetivo;
+}
+
+int Rutina::getDuracionMin() {
+    return duracionMin;
+}
+
+void Rutina::setDuracionMin(int duracionMin) {
+    this->duracionMin = duracionMin;
+}
+
+string Rutina::getDificultad() {
+    return dificultad;
+}
+
+void Rutina::setDificultad(string dificultad) {
+    this->dificultad = dificultad;
+}
+
+string Rutina::obtenerDescripcion() {
+    return nombre + " | Objetivo: " + objetivo + " | " + to_string(duracionMin) + " min | " + dificultad;
+}
 #endif //UNTITLED4_RUTINA_H

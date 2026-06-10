1caf250761be40772403584a49ef287402f771b5
Erick26m <a01715444@tec.mx>
2026-06-09 19:53:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,122 +11,12 @@ correcta dependiendo del tipo real del objeto.
 Al final se libera la memoria con delete porque los objetos fueron creados
 dinámicamente con new.
 */
-#include "estudiante.h"
-#include "profesor.h"
-#include <iostream>
-#include "inscripcion.h"
-#include "curso.h"
-
 using namespace std;
+#include "academia.h"
 
 int main() {
-    Inscripcion inscripcion1(
-        "Intensiva",
-        2500,
-        6
-    );
-
-    Inscripcion inscripcion2(
-        "Regular",
-        1800,
-        4
-    );
-
-    Curso curso1(
-        "Ingles Intermedio",
-        "Mejorar conversacion",
-        80,
-        "B1"
-    );
-
-    Curso curso2(
-        "Frances Basico",
-        "Aprender fundamentos",
-        60,
-        "A1"
-    );
-
-    Curso curso3(
-        "Ingles Avanzado",
-        "Preparacion TOEFL",
-        100,
-        "B2"
-    );
-
-    Estudiante* estudiante1 = new Estudiante(
-        "Armando",
-        19,
-        "A0001",
-        95,
-        "B1",
-        "Certificacion TOEFL",
-        inscripcion1
-    );
-
-    Estudiante* estudiante2 = new Estudiante(
-        "Luis",
-        20,
-        "A0002",
-        88,
-        "A1",
-        "Aprender frances desde cero",
-        inscripcion2
-    );
-
-    Estudiante* estudiante3 = new Estudiante(
-        "David",
-        21,
-        "A0003",
-        92,
-        "B2",
-        "Intercambio academico",
-        inscripcion1
-    );
-
-    Profesor* profesor1 = new Profesor(
-        "Erick",
-        30,
-        "A001",
-        "Ingles",
-        5
-    );
-
-    profesor1->asignarCurso(*estudiante1, curso1);
-    profesor1->asignarCurso(*estudiante2, curso2);
-
-    // Sobrecarga de metodo:
-    // Aqui se asigna un curso usando solo nombre y objetivo,
-    profesor1->asignarCurso(*estudiante3,"Curso de conversacion avanzada","Mejorar fluidez oral");
-
-    // Aplicacion de polimorfismo:
-    // Se crea un arreglo de apuntadores de tipo Persona.
-    // Aunque Persona es la clase madre abstracta, el arreglo guarda
-    // objetos de clases hijas: Estudiante y Profesor.
-    // Gracias al metodo virtual puro obtenerResumen(),
-    // se ejecuta la version correspondiente segun el tipo real del objeto.
-
-    Persona* personas[4];
-
-    personas[0] = estudiante1;
-    personas[1] = estudiante2;
-    personas[2] = estudiante3;
-    personas[3] = profesor1;
-
-    cout << "Oak-Language-Academy" << endl;
-    cout << endl;
-
-    for(int i = 0; i < 4; i++) {
-        cout << personas[i]->obtenerResumen() << endl;
-    }
-
-    // Liberacion de memoria:
-    // Como se usaron apuntadores con new, se eliminan con delete.
-    // El destructor virtual en Persona permite destruir correctamente
-    // los objetos de las clases hijas.
-
-    for(int i = 0; i < 4; i++) {
-        delete personas[i];
-    }
+    Academia academia;
+    academia.iniciar();
 
     return 0;
 }
